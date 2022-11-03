4447b058608344b36b2e373b5848b7f0c1047a4b
mmendell <105883493+mmendell@users.noreply.github.com>
2022-11-03 14:18:26 -0400

    task 5.1

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Alert } from "react-native";
+import "react-native-gesture-handler";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// screens that are navigated
+
+import Start from "./components/Start";
+import Chat from "./components/Chat";
+
+const Stack = createStackNavigator();
+
+export default class HelloWorld extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+  }
+
+  alertMyText(input = []) {
+    Alert.alert(input.text);
+  }
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Start">
+          <Stack.Screen name="Start" component={Start} />
+          <Stack.Screen name="Chat" component={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
