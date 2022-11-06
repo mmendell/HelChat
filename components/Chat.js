d48176b089301e2676833df949e1230168444942
mmendell <105883493+mmendell@users.noreply.github.com>
2022-11-06 11:04:28 -0500

    task 5.2: added gifted chat and stores message to state

--- components/Chat.js
+++ components/Chat.js
@@ -1,29 +1,77 @@
 import React from "react";
-import { View, Text } from "react-native";
+//imporrt requyired react native components
+import {  Platform, KeyboardAvoidingView, View } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
+
+// creating the chat module
 export default class Chat extends React.Component {
-  componentDidMount(){
-    // name property to display in the head
-    let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
+  constructor() {
+    super();
+    // creating the message state
+    this.state = {
+      messages: [],
+    };
+  }
 
+  // updating the message state to have default system messages
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+         },
+      ],
+    });
   }
 
 
+  // handle the sent messages
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+
+// forming ht eactual view
   render() {
-    // color property selced acts as background
+    // have the name sentered by user display ion top
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name});
     let color = this.props.route.params.color;
-    return(
-      <View
-      style={{
-        flex:1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: color,
-      }}
-      >
-        <Text>Chat window</Text>
-      </View>
-    )
+
+    return (
+    <View style={{flex:1}}>
+       <GiftedChat
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+
+        {/* ensure the keyboard isnt overlapped on android */}
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height"/>
+        ) : null}
+    </View>
+       
+
+  
+    );
   }
 }
